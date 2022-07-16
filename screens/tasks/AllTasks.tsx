d002b6275a7192eb28d921a20d8b04f5325db6b4
Emrulla Ramilli <emrulla.ramilli@softup.co>
2022-07-16 21:58:05 +0200

    all task fixes

--- screens/tasks/AllTasks.tsx
+++ screens/tasks/AllTasks.tsx
@@ -5,11 +5,37 @@ import { getAllTaks } from "../../api/task";
 import { ActivityIndicator } from "react-native-paper";
 import SingleTask from "./components/SingleTaskComponent";
 import useTheme from "../../common/hooks/useTheme";
-
+import { FontAwesome } from "@expo/vector-icons";
 const AllTasks = () => {
   const { colors } = useTheme();
   const { data: tasks, isLoading, isError } = useQuery("allTasks", getAllTaks);
 
+  const header = (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 40,
+      }}
+    >
+      <Text style={{ fontFamily: "poppinsBold", fontSize: 16 }}>
+        Here is the list of all tasks
+      </Text>
+      <FontAwesome
+        name="hand-pointer-o"
+        size={30}
+        color="black"
+        style={{
+          transform: [
+            {
+              rotate: "180deg",
+            },
+          ],
+        }}
+      />
+    </View>
+  );
   return (
     <View style={[styles.container, { backgroundColor: colors.primaryBg }]}>
       {isLoading && (
@@ -25,12 +51,15 @@ const AllTasks = () => {
           <Text>No Tasks to show :(</Text>
         </View>
       )}
+
       <FlatList
-        numColumns={2}
+        numColumns={1}
         data={tasks}
         keyExtractor={(item) => item.id.toString()}
+        ListHeaderComponent={header}
+        ListFooterComponent={<View style={{ height: 20 }}></View>}
         renderItem={({
-          item: { date, shouldNotify, title, description, id },
+          item: { date, shouldNotify, title, description, id, image },
         }) => (
           <SingleTask
             id={id}
@@ -38,6 +67,7 @@ const AllTasks = () => {
             title={title}
             description={description}
             shouldNotify={shouldNotify}
+            image={image ? image : ""}
           />
         )}
       />
